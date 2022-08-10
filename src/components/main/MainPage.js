998dd4720978aa1ba3303f3271760785e592c800
its-your-nuna <200103140@stu.sdu.edu.kz>
2022-08-10 10:40:00 +0600

    change function onload

--- src/components/main/MainPage.js
+++ src/components/main/MainPage.js
@@ -42,43 +42,40 @@ export const MainPage = ({ count,setCount,player }) => {
   }, [tasksDone])
 
 
-  function onload () {
-
-    var 
-      timer = new CountDownTimer(10);
-
-    timer.onTick(format).onTick(restart).start();
-    function restart() {
-      if (daysCount < 21) {
-        if (this.expired()) {
-           setTimeout(function () {
-            timer.start()
-          }, 1000);
-          daysCount += 1;
-          setDaysCount(daysCount)
-          setTasksDone(false)
-          setIsExpired(true)
-        }
-
-      } else if (this.expired() && daysCount > 3) {
-        setTimeout(function () {
-          ;
-        }, 1000);
-        setDaysCount(0)
-      }
-  
-    }
-    function format(minutes, seconds) {
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
-      setTime( minutes + ':' + seconds);
-    }
-  };
+ 
   
   useEffect(()=>{
-    return ()=>{
-      onload()
-    }
+    return function onload () {
+
+      var timer = new CountDownTimer(5);
+  
+      timer.onTick(format).onTick(restart).start();
+      function restart() {
+        if (daysCount < 21) {
+          if (this.expired()) {
+             setTimeout(function () {
+              timer.start()
+            }, 1000);
+            daysCount += 1;
+            setDaysCount(daysCount)
+            setTasksDone(false)
+            setIsExpired(true)
+          }
+  
+        } else if (this.expired() && daysCount > 3) {
+          setTimeout(function () {
+            ;
+          }, 1000);
+          setDaysCount(0)
+        }
+    
+      }
+      function format(minutes, seconds) {
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        setTime( minutes + ':' + seconds);
+      }
+    };
   },[])
   
   return (
